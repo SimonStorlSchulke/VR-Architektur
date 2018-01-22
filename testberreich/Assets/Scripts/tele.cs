a47be772bc064666186d0b31e3af8eb047d4d71b
Chris <christoph@latocha.de>
2018-01-22 01:16:01 +0100

    interagierbar

---
+++ testberreich/Assets/Scripts/tele.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class tele : MonoBehaviour {
+
+    [SerializeField]private string index;
+
+    private void OnTriggerEnter(Collider other)
+    {
+           
+            SceneManager.LoadScene(index);
+        
+    }
+
+
+
+
+
+
+
+}
